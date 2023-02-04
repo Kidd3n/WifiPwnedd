8a467eed077f7c1c69d8c9bfc205b26612983ca3
Ruben Gomez <santiagogomezgarro@gmail.com>
2023-02-04 11:36:48 -0600

    fix

--- WifiCrack.sh
+++ WifiCrack.sh
@@ -45,7 +45,7 @@ if [ $airtest -eq 0 ]; then
 			airmon-ng check kill > /dev/null 2>&1
 			echo -e "\n${greenColour}[*] Ya tienes tu tarjeta preparada!"
 		read -p "Quieres continuar? [Y/N]: " rps
-			if [ "rps" == "Y" ] || [ "rps" == "y" ]; then
+			if [ "$rps" == "Y" ] || [ "$rps" == "y" ]; then
 				echo -e "${purpleColour}"
 				read -p "Nombre de la red wifi: " wifi
 				$cleancolor
