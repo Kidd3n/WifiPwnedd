4e40f5231baa825c59ced43fd76488fce965d414
Ruben Gomez <santiagogomezgarro@gmail.com>
2023-03-07 19:41:42 -0600

    fix

--- WifiPwnedd.sh
+++ WifiPwnedd.sh
@@ -165,7 +165,7 @@ handshake_ataque() {
    	xterm -e "airodump-ng -w /tmp/xtermNeT --output-format csv ${tar}" &
 	xtermnet=$!
 	sleep 0.5; kill -9 $xtermnet; wait $xtermnet 2>/dev/null
-	echo -e "${blueColour}->->->->-> Select a network <-<-<-<-<-\n\n\n"
+	echo -e "${blueColour}->->->->-> Select a network <-<-<-<-<-${endColour}\n\n\n"
 	while IFS= read -r line; do
 		macrepeat=$(grep $line /tmp/xtermNeT-01.csv | wc -l)
 		if [[ "$macrepeat" > 1 ]]; then
@@ -174,7 +174,7 @@ handshake_ataque() {
 	done < <(cat /tmp/xtermNeT-01.csv | sed '1,2d' | cut -d "," -f 1 | sed '/Station/,1d')
 
 	launch_irodump=$(cat /tmp/xtermNeT-01.csv | sed '1,2d' | cut -d "," -f 1,6,4,9,14 | sed 's/,/   /g; /Station/,$d' | sed '$d' | nl -w3 -s "]    " | sed 's/[0-9]/[&/' | sed "s/\[/${rojo}\[${extrojo}/g; s/\]/${rojo}\]${extrojo}/g " | sed 's/WPA2 WPA/WPA2/;s/         -/        -/;s/        -/            -/;s/[0-9][0-9][0-9]    /&-/;s/    -W/   W/g;s/    -        -/           -/g;s/WEP    /WEP     /;s/WEP      /WEP     /; s/WPA    /WPA     /;s/WPA      /WPA     / ; s/OPN    /OPN     /; s/OPN      /OPN     /; s/    $/    (Hidden Wifi)/; s/ -1    (/ -1     (/;s/    ++/    /; s/    +/    /;s/+//g')
-	echo -e "           Bssid        CH    Encry   PWR       Essid\n----------------------------------------------------------------------\n$launch_irodump"
+	echo -e "               Bssid          CH      Encry   PWR       Essid\n----------------------------------------------------------------------\n$launch_irodump"
 	echo -ne "\n$greenColour[?]$grayColour Select a network: " && read ap
 	echo -ne "${greenColour}[?]$grayColour What channel is ${ap}?: " && read channel
 	tput civis
