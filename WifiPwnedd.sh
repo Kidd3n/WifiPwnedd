6a1425662216b5067d0c5a3b49f91aa3567a24b6
Ruben Gomez <santiagogomezgarro@gmail.com>
2023-03-11 14:01:46 -0600

    fix banner attack menu

--- WifiPwnedd.sh
+++ WifiPwnedd.sh
@@ -547,11 +547,11 @@ else
 				echo -e "${redColour}"
 				echo -e "	       .--======."
 				echo -e "     :      .-@#*==-==+#@*-."
-				echo -e "     I__________$$%%#*-.___"
+				echo -e "     I__________%@%%#*-.___"
 				echo -e "O====I__________#%%#*-._____>"
 				echo -e "     I    .-*=:::::--."
 				echo -e "     :      .-####-."
-				echo -e "	        .*_"
+				echo -e "	      .*_"
 				echo -e "               ."
 				sleep 0.5
 				echo -e "${greenColour}\n[+]${grayColour} Network card: $tar"
