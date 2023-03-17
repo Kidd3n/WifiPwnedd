2c0271c38a115099f420239d81ec0d45ad67ec01
Ruben Gomez <santiagogomezgarro@gmail.com>
2023-03-17 17:01:25 -0600

    tputs

--- wifipwnedd.sh
+++ wifipwnedd.sh
@@ -153,7 +153,7 @@ programs() {
 }
 #Update your OS repositories
 updatepackages() {
-	tput cnorm; clear; echo -ne "\n${blueColour}[?]$grayColour Do you want to update the packages? [Y/N]: " && read update 
+	tput cnorm; echo -ne "\n${blueColour}[?]$grayColour Do you want to update the packages? [Y/N]: " && read update 
 	if [ "$update" == "y" ] || [ "$update" == "Y" ]; then
 		if [ "$debian" -eq 0 ]; then
 			clear; tput civis
@@ -184,7 +184,7 @@ modeverification() {
 	iwconfig | grep Monitor > /dev/null 2>&1
 	if [ "$(echo $?)" -ne 0 ]; then
 		echo -e "\n$redColour[!]$grayColour Monitor mode is not activated"
-		sleep 2; echo -ne "$greenColour[?]$grayColour You want to reactivate the monitor mode? [Y/N]: " && read again
+		sleep 2; tput cnorm; echo -ne "$greenColour[?]$grayColour You want to reactivate the monitor mode? [Y/N]: " && read again
 		if [ "$again" == "y" ] || [ "$again" == "Y" ]; then
 			monitormode
 		elif [ "$again" == "n" ] || [ "$again" == "N" ]; then
@@ -203,6 +203,7 @@ monitormode() {
 	airmon-ng start $tar > /dev/null 2>&1
 	clear; echo -e "\n${blueColour}[*]$grayColour Interface:\n" && iwconfig 
 	tput cnorm; echo -ne "${redColour}\n[?]$grayColour Card confirmation (Enter the name exactly as it appears): " && read tar
+	tput civis
 	airmon-ng check kill > /dev/null 2>&1
 	ifconfig $tar down > /dev/null 2>&1 && macchanger -a $tar > /dev/null 2>&1
 	ifconfig $tar up > /dev/null 2>&1
